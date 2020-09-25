4cb82d5f57f0b96ac477f1984925254bb5a97e6a
Ahmad <s19047@pjwstk.edu.pl>
2020-09-25 18:40:40 +0200

    initial commit

---
+++ APBD1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace APBD1
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var url = "https://support.google.com/mail/thread/7926745?hl=en";
+			var httpClient = new HttpClient();
+
+			var response = await httpClient.GetAsync(url);
+
+			if (response.IsSuccessStatusCode)
+			{
+				var body = await response.Content.ReadAsStringAsync();
+				var emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+
+				var emails = emailRegex.Matches(body);
+
+				foreach(var email in emails)
+				{
+					Console.WriteLine(email);
+				}
+			}
+
+
+		}
+	}
+}
